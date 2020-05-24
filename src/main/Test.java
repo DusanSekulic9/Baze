65b7661dd4514d719fab97eaac4dc80b0a4ad9c9
ZekaUgr <65835412+ZekaUgr@users.noreply.github.com>
2020-05-24 19:45:45 +0200

    Promena

--- src/main/Test.java
+++ src/main/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Testiram git");
 		System.out.println("Test2");
+		System.out.println("Adadadadadadadada");
 	}
 }
